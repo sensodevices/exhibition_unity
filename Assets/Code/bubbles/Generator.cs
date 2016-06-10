a7c197737741a37bc29112a3b388ed930d5e309c
Sergey Birukov <Sergey Birukov>
2016-06-10 09:14:31 +0700

    Demo scene preparation

---
+++ Assets/Code/bubbles/Generator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+	public GameObject[] spawnableObjects;
+	public Transform spawnPoint;
+
+	private GameObject spawnedObject;
+	private System.Random numberGenerator;
+	private Vector3 spawnPosition;
+	private Quaternion spawnRotation;
+
+	// Use this for initialization
+	void Start () {
+		numberGenerator = new System.Random();
+		if (spawnPoint != null) {
+			spawnPosition = spawnPoint.position;
+			spawnRotation = spawnPoint.rotation;
+		} else {
+			spawnPosition = Vector3.zero;
+			spawnRotation = Quaternion.identity;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spawnedObject == null) {
+			var newInd = numberGenerator.Next(spawnableObjects.Length);
+			spawnedObject = (GameObject)Instantiate(spawnableObjects[newInd], spawnPosition, spawnRotation);
+			spawnedObject.transform.parent = transform;
+		}
+	}
+}
