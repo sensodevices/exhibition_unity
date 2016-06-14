27c280170a65948ef1d63df201b82626b17b653c
Sergey Birukov <Sergey Birukov>
2016-06-14 18:04:31 +0700

    TCP client for Senso VR data

--- Assets/Code/SensoGlove/NetworkManager.cs
+++ Assets/Code/SensoGlove/NetworkManager.cs
@@ -7,139 +7,212 @@ using SimpleJSON;
 
 public class NetworkManager : MonoBehaviour 
 {
-  private class UdpState 
-  {
-      public IPEndPoint e;
-      public UdpClient u;
-      public bool is_sending;
-  }
-    
-  private int listenPort {
-    get { return GameSettings.GetInboundPort(); }
-  }
-  private int vibratePort {
-    get { return GameSettings.GetVibratePort(); }
-  }
-  public int sensorsCnt = 7;
-  public Canvas GameSettingsMenu; // prefab
   public Transform VRCamera;
 
-  private UdpClient udpClient = null;
-  private UdpState udpState = null;
-  private IPEndPoint vibrateEP; // EndPoint to send vibrate packets to
-	
-  private HandNetworkData handData; // Storage for hands data
+  private class TcpState 
+  {
+      public TcpClient sock;
+      public NetworkStream stream;
+      public Byte[] inBuffer;
+
+      public int inBufferOffset;
       
-  // Incoming buffer control
-  private byte[] inBuffer;
-  private int inBufferOffset;
+      public bool ready;
+      public bool is_sending;
+      public bool waitConnect;
+      public DateTime lastConnectRetry;
+  }
+   
+  private string sensoHost {
+    get { return "192.168.15.79"; }
+  } 
+  private int sensoPort {
+    get { return 13456; }
+  }
+  public Canvas GameSettingsMenu; // prefab
+    
+  private TcpState tcpState;
+  private HandNetworkData handData; // Storage for hands data
+  private AsyncCallback m_ConnectCallback, m_SendCb, m_ReadCb;
+
+  private DateTime lastVRCameraSent = DateTime.Now;
   
-  private AsyncCallback m_RcvCb;
-  private AsyncCallback m_SendCb;
-	
 	void Start ()
   {
-    inBuffer = new byte[1024];
-    inBufferOffset = 0;
-    
   	handData = new HandNetworkData();
-		startUdpListener();
+
+    m_ConnectCallback = new AsyncCallback(ConnectCallback);
+    m_SendCb = new AsyncCallback(SendCallback);
+    m_ReadCb = new AsyncCallback(ReceiveCallback);
+
+    tcpState = new TcpState();    
+    tcpState.inBufferOffset = 0;
+    tcpState.inBuffer = new Byte[1024];
+    tcpState.ready = false;
+    tcpState.is_sending = false;
+    do_connect();
   }
 
-  void FixedUpdate ()
+  private void do_connect()
   {
-    if (vibrateEP != null && udpState.is_sending == false) {
-      var rotationArray = new float[] { VRCamera.rotation.w, VRCamera.rotation.x, VRCamera.rotation.y, VRCamera.rotation.z };
-      byte[] msg = new byte[19];
-      msg[0] = 0x45;
-      msg[1] = 0x67;
-      msg[2] = 0x02;
-      Buffer.BlockCopy(rotationArray, 0, msg, 3, rotationArray.Length * 4);
+    tcpState.lastConnectRetry = DateTime.Now;
+    IPAddress anIP;
 
-      udpState.is_sending = true;
-      udpClient.BeginSend(msg, 3, vibrateEP, m_SendCb, udpState);
+    if (IPAddress.TryParse(sensoHost, out anIP)) {
+      tcpState.waitConnect = true;
+      if (tcpState.sock == null) {
+        tcpState.sock = new TcpClient(AddressFamily.InterNetwork);
+      }
+      tcpState.sock.BeginConnect(anIP, sensoPort, m_ConnectCallback, tcpState);
+    } else {
+      Debug.LogError("Unable to parse senso server IP address");
     }
+  }
+  
+  public void Restart()
+  {
+    //TODO: Restart network manager
+    Debug.Log("Restart network manager");
+    Stop();
+    do_connect();
+  }
+
+  public void Stop()
+  {
+    if (tcpState.stream != null) {
+      tcpState.stream.Close();
+    }
+    tcpState.sock.Close();
+    tcpState.sock = null;
+    tcpState.ready = false;
+    tcpState.is_sending = false;
+
+    tcpState.inBufferOffset = 0;
   }
 
   void OnDestroy() 
   {
-    stopUdpListener();
+    Stop();
 	}
   
-  private void startUdpListener()
+  void FixedUpdate()
   {
-    IPEndPoint e = new IPEndPoint(IPAddress.Any, listenPort);
-    udpClient = new UdpClient(e);
-    udpState = new UdpState();
-    udpState.e = e;
-    udpState.u = udpClient;
-    udpState.is_sending = false;
-    m_RcvCb = new AsyncCallback(ReceiveCallback);
-    udpClient.BeginReceive(m_RcvCb, udpState);
-    
-    m_SendCb = new AsyncCallback(SendCallback);
-    vibrateEP = GameSettings.GetVibrateEndpoint();
-  }
-  
-  private void stopUdpListener()
-  {
-    udpClient.Close();
-    udpClient = null;
-    udpState = null;
-  }
-  
-  /// <summary>Restarts Udp socket with binding to a port stored in game settings</summary>
-  public void Restart()
-  {
-    if (udpClient != null) {
-      stopUdpListener();
-    }
-    startUdpListener();
-  }
-    
-  private void ReceiveCallback(IAsyncResult ar) 
-  {
-    UdpClient u = (UdpClient)((UdpState)(ar.AsyncState)).u;
-    IPEndPoint e = (IPEndPoint)((UdpState)(ar.AsyncState)).e;
+    if (tcpState.ready) {
+      if (tcpState.sock.Client.Connected) {
+        /*if (tcpState.sock.Client.Poll(1, SelectMode.SelectRead) && tcpState.sock.Client.Available == 0) {
+          HandleDisconnect(1);
+          return;
+        }*/
+      } else {
+        HandleDisconnect(2);
+        return;
+      }
+      var now = DateTime.Now;
+      var dt = now - lastVRCameraSent;
+      if (!tcpState.is_sending && dt.TotalMilliseconds >= 100) {
+        var rotationArray = new float[] { VRCamera.rotation.w, VRCamera.rotation.x, VRCamera.rotation.y, VRCamera.rotation.z };
+        byte[] msg = new byte[19];
+        msg[0] = 0x45;
+        msg[1] = 0x67;
+        msg[2] = 0x02;
+        Buffer.BlockCopy(rotationArray, 0, msg, 3, rotationArray.Length * 4);
 
-    Byte[] receiveBytes = u.EndReceive(ar, ref e);
-    Buffer.BlockCopy(receiveBytes, 0, inBuffer, inBufferOffset, receiveBytes.Length);
-    inBufferOffset += receiveBytes.Length;
-    
-    for (int i = inBufferOffset - 1; i > 4; --i) {
-      if (inBuffer[i - 3] == 0x7D && inBuffer[i - 2] == 0x5D && inBuffer[i - 1] == 0x7D && inBuffer[i] == 0x7D) {
-        // found end of packet
-        String text = Encoding.UTF8.GetString(inBuffer, 0, i + 1);
-        JSONNode parsedPacket = null;
+        tcpState.is_sending = true;
         try {
-          parsedPacket = JSON.Parse(text);
-        } catch (Exception je) {
-          Debug.Log("Error parsing: " + je.Message);
+          tcpState.stream.BeginWrite(msg, 0, 19, m_SendCb, tcpState);
+        } catch (Exception err) {
+          HandleDisconnect(3);
+        } finally {
+          lastVRCameraSent = now;
         }
-        if (parsedPacket != null) {
-          var leftHand = parsedPacket["lh"];
-          if (leftHand != null )
-            handData.DeserializeHand(HandNetworkData.DataType.LeftHand, leftHand.AsObject);
-
-          var rightHand = parsedPacket["rh"];
-          if (rightHand != null )
-            handData.DeserializeHand(HandNetworkData.DataType.RightHand, rightHand.AsObject);
-        }
-        
-        if (i + 1 < inBufferOffset) {
-          Buffer.BlockCopy(inBuffer, i + 1, inBuffer, 0, inBufferOffset - i - 1);
-          inBufferOffset -= (i - 1);
-          text = Encoding.UTF8.GetString(inBuffer, 0, inBufferOffset);
-        } else {
-          inBufferOffset = 0;
-        }
-        i = inBufferOffset - 1;
+      }
+    } else if (!tcpState.waitConnect) {
+      var diff = DateTime.Now - tcpState.lastConnectRetry;
+      if (diff.TotalSeconds > 5) {
+        do_connect();
       }
     }
-    udpClient.BeginReceive(m_RcvCb, udpState);
+  }
+  
+  /// <summary>Handler for connect event</summary>  
+  private void ConnectCallback(IAsyncResult ar) 
+  {
+    var aState = (TcpState)ar.AsyncState;
+    aState.waitConnect = false;
+    var tcpClient = (TcpClient)aState.sock;
+    Debug.Log("connected: " + tcpClient.Connected);
+    if (tcpClient.Connected) {
+      tcpState.stream = tcpClient.GetStream();
+      tcpState.ready = true;
+      do_read(tcpState);
+    }
   }
 
+  private void SendCallback(IAsyncResult ar)
+  {
+    ((TcpState)(ar.AsyncState)).is_sending = false;
+  }
 
+  private void ReceiveCallback(IAsyncResult ar)
+  {
+    var aState = (TcpState)(ar.AsyncState);
+    int read = aState.stream.EndRead(ar);
+    if (read == 0) { HandleDisconnect(4); return; }
+    //
+    aState.inBufferOffset += read;
+
+    int msgEnd = aState.inBufferOffset - 1, msgStart = 0;
+    for ( ; msgEnd >= 0; --msgEnd)
+    {
+      if (aState.inBuffer[msgEnd] == '\0') {
+        for (msgStart = msgEnd - 1; msgStart >= 0; --msgStart) {
+          if (aState.inBuffer[msgStart] == '\0') {
+            break;
+          }
+        }
+        break;
+      }
+    }
+    if (msgStart == -1) msgStart = 0;
+
+    if (msgEnd != msgStart) {
+      String text = Encoding.UTF8.GetString(aState.inBuffer, msgStart, msgEnd - msgStart);
+          
+      JSONNode parsedPacket = null;
+      try {
+        parsedPacket = JSON.Parse(text);
+      } catch (Exception je) {
+        Debug.Log("Error parsing: " + je.Message);
+      }
+      if (parsedPacket != null) {
+        var leftHand = parsedPacket["lh"];
+        if (leftHand != null )
+          handData.DeserializeHand(HandNetworkData.DataType.LeftHand, leftHand.AsObject);
+
+        var rightHand = parsedPacket["rh"];
+        if (rightHand != null )
+          handData.DeserializeHand(HandNetworkData.DataType.RightHand, rightHand.AsObject);
+      }
+
+      if (msgEnd < aState.inBufferOffset) {
+        Buffer.BlockCopy(aState.inBuffer, msgEnd + 1, aState.inBuffer, 0, aState.inBufferOffset - msgEnd - 1);
+        aState.inBufferOffset = aState.inBufferOffset - msgEnd - 1;
+      } else {
+        aState.inBufferOffset = 0;
+      }
+    }
+    do_read(aState);
+  }
+
+  private void do_read(TcpState aState)
+  {
+    try {
+      aState.stream.BeginRead(aState.inBuffer, aState.inBufferOffset, aState.inBuffer.Length - aState.inBufferOffset, m_ReadCb, aState);
+    } catch (Exception err) {
+      HandleDisconnect(5);
+    }
+  }
+  
   public HandNetworkData.SingleHandData GetHandData(HandNetworkData.DataType handType)
   {
     if (handData == null) return null;
@@ -149,27 +222,24 @@ public class NetworkManager : MonoBehaviour
       return handData.rightHand;
     }
   }
-  
+
   public void VibrateFinger(HandNetworkData.DataType handType, byte fingerId, byte duration)
   {
-    if (vibrateEP != null && udpState.is_sending == false) {
+    if (tcpState.ready && !tcpState.is_sending) {
       byte[] msg = new byte[4];
       msg[0] = 0x45;
       msg[1] = 0x67;
       msg[2] = 0x01;
       msg[3] = (byte)((fingerId << 5) | (duration & 0x1F));
-      
-      udpState.is_sending = true;
-      udpClient.BeginSend(msg, 3, vibrateEP, m_SendCb, udpState);
-      // udpClient.Send(msg, 3, vibrateEP);
+      tcpState.is_sending = true;
+      try {
+        tcpState.stream.BeginWrite(msg, 0, 4, m_SendCb, tcpState);
+      } catch (Exception err) {
+        HandleDisconnect(6);
+      }
     }
   }
-  
-  private void SendCallback(IAsyncResult ar)
-  {
-    ((UdpState)(ar.AsyncState)).is_sending = false;
-  }
-  
+
   /// <summary>Opens a menu to set up the Senso runtime</summary>
 	public void OpenSettings() 
 	{
@@ -178,4 +248,12 @@ public class NetworkManager : MonoBehaviour
 		setMan.netManager = this.GetComponent<NetworkManager>();
 	}
 
+
+  private void HandleDisconnect(int reason)
+  {
+    if (tcpState.ready) {
+      Debug.Log("Disconnect " + reason);
+      Stop();
+    }
+  }
 }
