7bb53fc536e9f431609de0467bbdbd6c54141287
Sergey Birukov <Sergey Birukov>
2016-06-24 12:01:19 +0700

    Action menu scroll

--- Assets/Code/GalaxyChat/Ui/MenuActions.cs
+++ Assets/Code/GalaxyChat/Ui/MenuActions.cs
@@ -28,20 +28,24 @@ public class MenuActions : MonoBehaviour {
 
 	List<MenuItem> items = new List<MenuItem>();
 	
+	private static float FingerMoveMax = 0.05f;
+
+	private FingerTarget indexFinger;
+	private float lastScrollUpdated = 0;
 	
 	void Awake(){
 		Me = this;
 		DismissLocal();// прячем
+		indexFinger = GlobalFuncs.GetFinger(HandNetworkData.DataType.RightHand, HandNetworkData.FingerType.Index);
 	}
-	
-	void Update(){
-		var dir = 0f; 
-		if (Input.GetKey(KeyCode.UpArrow)){
-			dir = 0.1f;
-		} else if (Input.GetKey(KeyCode.DownArrow)){
-			dir = -0.1f;
-		}
-		scrollRect.verticalNormalizedPosition += dir*Time.deltaTime;
+
+	void Update() {
+		var angle = indexFinger.GetRelativeAngle() - 10;
+		var dir = 0.5f - angle / 130.0f;
+		if (Mathf.Abs(dir) < 0.10f) return;
+		float sign = Mathf.Abs(dir) / dir;
+		dir *= dir * sign;
+		scrollRect.verticalNormalizedPosition += dir * Time.deltaTime;
 	}
 
 	public static void AddItem (string text, string action) {
@@ -54,11 +58,6 @@ public class MenuActions : MonoBehaviour {
 	
 	public static void Show (int associatedUserId, string userName) {
 		Me.ShowLocal(associatedUserId, userName);
-
-		var indexFinger = GlobalFuncs.GetFinger(HandNetworkData.DataType.RightHand, HandNetworkData.FingerType.Index);
-		if (indexFinger != null) {
-			Debug.Log("index finger found");
-		}
 	}
 	
 	public void ShowLocal (int associatedUserId, string userName) {
@@ -108,5 +107,16 @@ public class MenuActions : MonoBehaviour {
 	void InvokeItemClick(MenuItem item){
 		if (OnItemClick != null)
 			OnItemClick(item);
+	}
+
+	public void onIndexFingerMoved(object finger, FingerMovedArgs args) {
+		// Debug.Log("finger moved: " + args.deltaMove.y);
+		var dy = args.deltaMove.y;
+		var abs = Mathf.Abs(dy); 
+		if (abs > 0.001f && abs < FingerMoveMax) {
+			if (dy < -FingerMoveMax) return; //dy = -FingerMoveMax;
+			else if (dy > FingerMoveMax) return; //dy = FingerMoveMax;
+			scrollRect.verticalNormalizedPosition += dy;
+		}
 	}
 }
--- Assets/Code/SensoGlove/Fingers/FingerTarget.cs
+++ Assets/Code/SensoGlove/Fingers/FingerTarget.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 using System;
 
 public class FingerMovedArgs : EventArgs {
-	
+	public Vector3 deltaMove { get; private set; }
+	public FingerMovedArgs(Vector3 delta) {
+		deltaMove = delta;
+	}
 }
 
 public class FingerTarget : MonoBehaviour
@@ -17,11 +20,12 @@ public class FingerTarget : MonoBehaviour
 	private bool isVibrating { get { return m_isVibrating; } }
 	private bool m_isVibrating = false;
 	private byte m_vibrateStrength = 2;
-
 	private DateTime lastSent;
+	private Vector3 m_lastPosition;
+	private int m_moveSubscribed = 0;
 	
 
-	public event EventHandler<FingerMovedArgs> onMove = delegate {}; // Is fired when finger has moved
+	public event EventHandler<FingerMovedArgs> OnMove; // Is fired when finger has moved
 
 	public void RegisterEventEmitter(SensoEventEmitter evEmitter)
 	{
@@ -37,7 +41,23 @@ public class FingerTarget : MonoBehaviour
 				lastSent = DateTime.Now;
 			}
 		}
+		if (true) /*m_moveSubscribed > 0)*/ {
+			Vector3 palmPos = Vector3.zero;
+			if (m_palm.IsAlive) {
+				PalmTarget p = m_palm.Target as PalmTarget;
+				palmPos = p.transform.position;
+			}
+			Vector3 myPos = (transform.position - palmPos);
+			Vector3 deltaPos = myPos - m_lastPosition;
+			/*var arg = new FingerMovedArgs(deltaPos);
+			OnMove(this, arg);*/
+			m_lastPosition = myPos;
+		}
+
 	}
+
+	public void AddMoveSubscriber() { ++m_moveSubscribed; }
+	public void RemoveMoveSubscriber() { --m_moveSubscribed; if (m_moveSubscribed < 0) m_moveSubscribed = 0; }
 
 	public void SetNetworkManager(NetworkManager netMan) {
 		m_netMan = netMan;
